298ae41d73f1e6db18f185815bcf883d5aa22ac4
happylwp <18804207538@163.com>
2018-08-01 20:08:43 +0800

    add icon

--- app/Screen/Message/MessageScreen.js
+++ app/Screen/Message/MessageScreen.js
@@ -6,8 +6,8 @@ import { BackColor } from '../../utils/GlobleStyle'
 @connect(({ MESSAGE }) => ({ ...MESSAGE }))
 class MessageScreen extends Component {
   static navigationOptions = () => ({
-    title: '消息'
-  });
+    title: '消息',
+  })
 
   render() {
     return (
